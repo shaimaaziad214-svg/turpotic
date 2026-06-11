74b96e3d3a179b418c57c275543e1e0f5afb972f
shaimaa ziad <shaimaaziad214@gmail.com>
2026-06-11 22:42:17 +0300

    GitHub Pages Fix

--- app/payments/page.tsx
+++ app/payments/page.tsx
@@ -3,6 +3,7 @@
 
 import { useState } from "react";
 import Image from "next/image";
+
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/turpotic",
+  assetPrefix: "/turpotic/",
 };
 
 export default nextConfig;

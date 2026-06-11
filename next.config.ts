720d2e4ab79f308a6fcca3bbdd1e5f5aae4b601d
shaimaa ziad <shaimaaziad214@gmail.com>
2026-06-11 21:48:11 +0300

    Fix Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/turpotic",
-  assetPrefix: "/turpotic/",
+  /* config options here */
 };
 
 export default nextConfig;
